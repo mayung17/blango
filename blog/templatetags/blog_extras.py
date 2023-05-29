f216e59c80ec9871dd65c9e234bb0026c78d176d
Mayung Rai <mayungrai178@gmail.com>
2023-05-29 23:28:39 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,6 +3,7 @@ from django import template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+from blog.models import Post
 register = template.Library()
 user_model = get_user_model()
 
@@ -18,9 +19,52 @@ def author_details(author, current_user):
     name = (f"author.username")
   
   if author.email:
-    prefix = format_html(f'<a href="mailto:{email}">')
-    suffix= format_html("</a>")
+    prefix = format_html('<a href="mailto:{}">',
+    author.email)
+    suffix = format_html("</a>")
+
   else:
     prefix=""
     suffix=""
   return format_html('{}{}{}', prefix, name, suffix)
+
+
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+  return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+  return format_html('<div class="column {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+  return format_html("</div>")
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
+
+# def author_details(author, current_user):
+#   if not isinstance(author, user_model):
+#   # return empty string as safe default
+#   return ""
+#   if author == current_user:
+#     return format_html("<strong>me</strong>")
+#   if author.first_name and author.last_name:
+#     name = f"{author.first_name} {author.last_name}"
+#   else:
+#     name = f"{author.username}"
+#   if author.email:
+#     prefix = format_html('<a href="mailto:{}">',
+#     author.email)
+#   suffix = format_html("</a>")
+#   else:
+#   prefix = ""
+#   suffix = ""
+#   return format_html('{}{}{}', prefix, name, suffix)
