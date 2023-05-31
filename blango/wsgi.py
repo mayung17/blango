0ef5f26b7034292a48491176bca847cd8a77bcbb
Mayung Rai <mayungrai178@gmail.com>
2023-05-31 09:10:50 +0000

    Finish django configurations

--- blango/wsgi.py
+++ blango/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blango.settings')
+#DEFINING DJANGO_CONFIGURATION
+os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
+
+#importing should be after we define DJANGO_CONFIGURATION
+from configurations.wsgi import get_wsgi_application
+
 
 application = get_wsgi_application()
