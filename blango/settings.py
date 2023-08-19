f937a97b14a7986cb47ecd2b58d211e37e976212
Mayung Rai <mayungrai178@gmail.com>
2023-08-19 04:20:27 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -47,14 +47,24 @@ class Dev(Configuration):
       'django.contrib.auth',
       'django.contrib.contenttypes',
       'django.contrib.sessions',
-      'django.contrib.messages',
       'django.contrib.staticfiles',
+      'django.contrib.messages',
+      'django.contrib.sites',
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
       'debug_toolbar',
-      'blango_auth'
+      'blango_auth',
+      'allauth',
+      'allauth.account',
+      'allauth.socialaccount',
+      'allauth.socialaccount.providers.google',
   ]
+  SITE_ID = 1
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
 
   MIDDLEWARE = [
       'debug_toolbar.middleware.DebugToolbarMiddleware',
